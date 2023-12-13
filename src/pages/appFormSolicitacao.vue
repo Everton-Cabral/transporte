3824b408b6a5d3b48bbf424850d63066eb0da7ed
Everton Cabral <90525060+Everton-Cabral@users.noreply.github.com>
2023-12-12 22:06:05 -0300

    inserindo tabela

--- src/pages/appFormSolicitacao.vue
+++ src/pages/appFormSolicitacao.vue
@@ -88,6 +88,7 @@
         <div class="form-materiais">
           <q-input rounded outlined v-model="codSap" label="Código SAP"  style="margin: 5px;"/>
           <q-input rounded outlined v-model="descricao" label="Descrição" style="margin: 5px;"/>
+          <q-input rounded outlined v-model="qtd" label="Qtd" style="margin: 5px;"/>
           <q-select rounded outlined v-model="unidade" :options="optionsUnidade" label="Unidade" style="margin: 5px;"/>
           <q-input rounded outlined v-model="comprimento" label="Comprimento  (m)" style="margin: 5px;"/>
           <q-input rounded outlined v-model="largura" label="Largura (m)" style="margin: 5px;"/>
@@ -97,6 +98,43 @@
           <q-btn round color="secondary" icon="add" style="margin: 5px; width: 60px;" @click="addMaterial()"/>
         </div>
       </div>
+
+      <div class="container-tabela">
+      <q-markup-table>
+        <thead>
+          <tr>
+            <th class="text-left">Código Sap</th>
+            <th class="text-right">Descrição</th>
+            <th class="text-right">Qtd</th>
+            <th class="text-right">Unidade</th>
+            <th class="text-right">Comprimento (m)</th>
+            <th class="text-right">Altura (m)</th>
+            <th class="text-right">Largura (m)</th>
+            <th class="text-right">Volume (m³)</th>
+            <th class="text-right">Peso (Kg)</th>
+            <th class="text-right">Peso Total (Kg)</th>
+            <th class="text-right">Estivação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(material, i) in materiais" :key="i">
+            <td class="text-left">{{ material.codSap }}</td>
+            <td class="text-right">{{ material.descricao }}</td>
+            <td class="text-right">{{ material.qtd }}</td>
+            <td class="text-right">{{ material.unidade }}</td>
+            <td class="text-right">{{ material.comprimento }}</td>
+            <td class="text-right">{{ material.largura }}</td>
+            <td class="text-right">{{ material.altura }}</td>
+            <td class="text-right">{{ material.volume }}</td>
+            <td class="text-right">{{ material.peso }}</td>
+            <td class="text-right">{{ material.pesoTotal }}</td>
+            <td class="text-right">{{ material.estivacao }}</td>
+          </tr>
+ 
+        </tbody>
+      </q-markup-table>
+  </div>
+      
   </div>
 </template>
 
@@ -128,6 +166,7 @@ export default {
       },
       codSap: '',
       descricao:'',
+      qtd:'',
       unidade: 'Unidade',
       optionsUnidade: ['Unidade', 'Caixa','Fardo'],
       comprimento:'',
@@ -153,11 +192,14 @@ export default {
       const novoMaterial = {
         codSap: this.codSap,
         descricao: this.descricao,
+        qtd: this.qtd,
         unidade: this.unidade,
         comprimento: this.comprimento,
         largura: this.largura,
         altura: this.altura,
         volume: this.comprimento * this.largura * this.altura,
+        peso: this.peso,
+        pesoTotal: this.qtd * this.peso,
         estivacao: this.estivacao,
       }
       this.materiais.push(novoMaterial)
@@ -309,6 +351,10 @@ export default {
 .form-materiais{
   display: flex;
   justify-content: center;
+}
+.container-tabela{
+  width: 95%;
+  margin: 10px;
 }
 @media (max-width: 1000px) {
     h2{
